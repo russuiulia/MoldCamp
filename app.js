0eb8463bb780b2518bb94228ca152156a92d5ace
russuiulia <russuiulia.md@gmail.com>
2024-11-24 20:25:09 +0200

    vercel

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ mongoose.connect(uri);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(__dirname + '/views'));
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/favicontest'));
 app.use(methodOverride('_method'));
